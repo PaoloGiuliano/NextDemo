11e6a4a2b8003229a5789a7fdfd16cbf2dd10c5c
Paolo <paolo14giuliano2001@gmail.com>
2025-03-07 17:50:51 -0500

    Update page.tsx

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -8,7 +8,7 @@ export default function PlayerPage() {
 
       {/* Example tags positioned at the bottom-right */}
       <div className="absolute bottom-4 right-4 text-gray-500">
-        <p className="text-sm sm:text-xs">Example Tags:</p>
+        <p className="text-[2px]">Example Tags:</p>
         <ul className="list-disc pl-5 text-sm sm:text-[3px]">
           <li>908PRLR</li>
           <li>98CJ8C09</li>
