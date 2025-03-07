141351690e4540f7483a90d2e853d2e3ef66fc2d
Paolo <paolo14giuliano2001@gmail.com>
2025-03-07 17:34:35 -0500

    Update getPlayerInfo.tsx

--- src/app/player/getPlayerInfo.tsx
+++ src/app/player/getPlayerInfo.tsx
@@ -27,7 +27,7 @@ export default function PlayerSearch() {
       } else {
         setPlayer(data);
       }
-    } catch (err) {
+    } catch {
       setError("Something went wrong.");
     }
   }
